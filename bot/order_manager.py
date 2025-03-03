64f5dfa90258cb94003911908ca7216602b6c83c
Ka0o0zz <Davidbate5025@gmail.com>
2025-03-02 20:31:54 -0500

    cambios

--- bot/order_manager.py
+++ bot/order_manager.py
@@ -37,12 +37,11 @@ class OrderManager:
         try:
             if order['filled'] == order['amount']:
                 side = 'sell' if order['side'] == 'buy' else 'buy'
-                target_price = format_price(
-                    order['price'] * (1 - self.percentage_spread if side == 'sell' else 1 + self.percentage_spread),
-                    self.price_format
-                )
-
+                target_price = order['price'] * (1 + self.percentage_spread if side == 'sell' else 1 - self.percentage_spread)
                 await self.create_order(side, order['amount'], target_price)
+                
+                #
+                await self.maintain_orders()
         except Exception as e:
             logging.error(f"Error procesando orden: {e}")
     
