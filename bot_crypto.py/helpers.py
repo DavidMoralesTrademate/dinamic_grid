6d94c0ecc1fb7a42ce096a38fc9975b20d8459e0
Ka0o0zz <Davidbate5025@gmail.com>
2025-03-22 07:48:41 -0500

    cambios

---
+++ bot_crypto.py/helpers.py
@@ -0,0 +1,15 @@
+def format_price(price, decimals=1):
+    return round(float(price), decimals)
+
+def calculate_order_prices_buy(price, percentage_spread, num_orders, decimals=1):
+    # Calcula precios decrecientes en un grid, por ejemplo
+    factor = 1 - percentage_spread
+    return [format_price(price * (factor ** i), decimals) for i in range(num_orders)]
+
+def calculate_order_prices_sell(price, percentage_spread, num_orders, decimals=1):
+    # Calcula precios decrecientes en un grid, por ejemplo
+    factor = 1 + percentage_spread
+    return [format_price(price * (factor ** i), decimals) for i in range(num_orders)]
+
+def format_quantity(quantity, decimals=2):
+    return round(float(quantity), decimals)
---
+++ bot_crypto.py/main.py
@@ -0,0 +1,24 @@
+from bot.core import BotMain
+
+CONFIG = {
+    'exchange_options': {
+        'apiKey': 'cxakp_ofHZL7daTQnJp93TqurJ3A',
+        'secret': 'Tu2ytzA9Sk1rsUeyUuGrrj', 
+    },
+    'exchange_name':'Crypto.com',
+    'account':'dm0012', 
+    'symbols': ['BTC/USD:USD'],
+    'amount': 10,
+    'percentage_spread': 0.0005,
+    'num_orders': 40,
+    'bias': 'long',
+    'price_format': 2,
+    'amount_format': 2,
+    'contract_size': 0.1,
+    'total_buys_filled': 0,
+    'total_sells_filled': 0,
+}
+
+if __name__ == "__main__":
+    bot = BotMain(CONFIG)
+    bot.run()
