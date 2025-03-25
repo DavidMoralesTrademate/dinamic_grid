697359720555e964101667df66bf61d16089112f
Ka0o0zz <Davidbate5025@gmail.com>
2025-03-24 23:11:53 -0500

    cambios

--- bot/order_manager.py
+++ bot/order_manager.py
@@ -40,9 +40,9 @@ class OrderManager:
         self.amount_format = config.get('amount_format')
         self.contract_size = config.get('contract_size')
 
-        self.total_buys_filled = 74552
-        self.total_sells_filled = 73561
-        self.match_profit = 4524.1025
+        self.total_buys_filled = 21321
+        self.total_sells_filled = 21223
+        self.match_profit = 3119.7670
 
         
 
