3f1b2cbc2d7532bfec18ce695b8f9110ca268d52
Ka0o0zz <Davidbate5025@gmail.com>
2025-03-31 10:10:33 -0500

    cambios

---
+++ init_15.py
@@ -0,0 +1,26 @@
+from bot.hola import BotMain
+
+CONFIG = {
+    'exchange_options': {
+        'apiKey': '658ca219-bd5d-42f1-8f80-ffe1edce2ed1',
+        'secret': 'DDF28BBD70C80C10B69225D991FE1DB5', 
+        'password': 'Bitcoin1.',
+    },
+    'exchange_name':'OKX',
+    'account':'dm0015', 
+    'symbols': ['BTC/USDT:USDT'],
+    'amount': 1000,
+    'contracts' : 0.9,
+    'percentage_spread': 0.0005,
+    'num_orders': 90,
+    'bias': 'long',
+    'price_format': 2,
+    'amount_format': 2,
+    'contract_size': 0.01,
+    'total_buys_filled': 0,
+    'total_sells_filled': 0,
+}
+
+if __name__ == "__main__":
+    bot = BotMain(CONFIG)
+    bot.run()
