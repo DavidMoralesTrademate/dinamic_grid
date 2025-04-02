681737397327b5abb9a1a9f267565dd3ab70f6bd
Ka0o0zz <Davidbate5025@gmail.com>
2025-04-02 13:47:50 -0500

    cambios

--- init_15.py
+++ init_15.py
@@ -12,7 +12,7 @@ CONFIG = {
     'amount': 16000,
     'contracts' : 100,
     'percentage_spread': 0.0005,
-    'num_orders': 90,
+    'num_orders': 60,
     'bias': 'long',
     'price_format': 2,
     'amount_format': 2,
