57ce744745da07be2a8b00eb949e147da393633f
Ka0o0zz <Davidbate5025@gmail.com>
2025-05-02 17:12:18 +0200

    cambios

--- init_crypto_btc.py
+++ init_crypto_btc.py
@@ -9,15 +9,15 @@ CONFIG = {
     'account':'Cuenta principal', 
     'symbols': ['BTC/USD:USD'],
     'amount': 10,
-    'percentage_spread': 0.05/100,
-    'num_orders': 20,
+    'percentage_spread': 0.1/100,
+    'num_orders': 30,
     'bias': 'short',
     'price_format': 1,
     'amount_format': 1,
-    'contract_size': 0.0001,
+    'contract_size': 0.0114,
     'total_buys_filled': 0,
     'total_sells_filled': 0,
-    'contracts': 0.0001
+    'contracts': 0.0114
 }
 
 if __name__ == "__main__":
